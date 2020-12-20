a8c53d168fd615faafeb57ee359cdff310907ed1
bangyidong <32199053+dby2017@users.noreply.github.com>
2020-12-21 00:17:03 +0800

    Update models.py

--- GAT/models.py
+++ GAT/models.py
@@ -26,32 +26,3 @@ class GAT(nn.Module):
         # 使用elu激活函数，进行非线性表达
         x = F.elu(self.out_att(x, adj))
         return F.log_softmax(x, dim=1)
-
-# 注意力机制的稀疏版本
-class SpGAT(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
-        """Sparse version of GAT."""
-        super(SpGAT, self).__init__()
-        self.dropout = dropout
-
-        self.attentions = [SpGraphAttentionLayer(nfeat, 
-                                                 nhid, 
-                                                 dropout=dropout, 
-                                                 alpha=alpha, 
-                                                 concat=True) for _ in range(nheads)]
-        for i, attention in enumerate(self.attentions):
-            self.add_module('attention_{}'.format(i), attention)
-
-        self.out_att = SpGraphAttentionLayer(nhid * nheads, 
-                                             nclass, 
-                                             dropout=dropout, 
-                                             alpha=alpha, 
-                                             concat=False)
-
-    def forward(self, x, adj):
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = F.elu(self.out_att(x, adj))
-        return F.log_softmax(x, dim=1)
-
